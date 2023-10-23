87dd4ea725c88f0025a0af7b7018b43476631935
Maciej Necka <macieknecka@gmail.com>
2023-10-23 20:49:46 +0200

    Upload data from excursions.json

--- src/js/client.js
+++ src/js/client.js
@@ -10,6 +10,69 @@ excursionsList.addEventListener('submit', addToCart);
 const orderForm = document.querySelector('.panel__order');
 orderForm.addEventListener('submit', handleOrderForm);
 
+async function loadExcursions() {
+  try {
+    const excursions = await excursionsAPI.fetchExcursions();
+    renderExcursionsList(excursions);
+  } catch (error) {
+    console.error(error);
+    alert('Błąd podczas ładowania danych');
+  }
+}
+loadExcursions();
+
+function renderExcursionsList(excursions) {
+  const excursionsList = document.querySelector('.panel__excursions');
+  excursionsList.innerHTML = '';
+
+  excursions.forEach((excursion) => {
+    const excursionItem = document.createElement('div');
+    excursionItem.className = 'excursions__item';
+    excursionItem.innerHTML = `
+  <li class="excursions__item excursions__item--prototype">
+          <header class="excursions__header">
+            <h2 class="excursions__title">${excursion.title}</h2>
+            <p class="excursions__description">
+              ${excursion.description}
+            </p>
+          </header>
+          <form class="excursions__form>
+            <div class="excursions__field">
+              <label class="excursions__field-name">
+                Dorosły:
+                <span class="excursions__field--adult-price">${excursion.adultPrice}</span> PLN x
+                <input
+                  class="excursions__field-input"
+                  name="adults"
+                  id="adults-input"
+                />
+              </label>
+            </div>
+            <div class="excursions__field">
+              <label class="excursions__field-name">
+                Dziecko:
+                <span class="excursions__field--child-price">${excursion.childPrice}</span> PLN x
+                <input
+                  class="excursions__field-input"
+                  name="children"
+                  id="children-input"
+                />
+              </label>
+            </div>
+            <div class="excursions__field excursions__field--submit">
+              <input
+                class="excursions__field-input excursions__field-input--submit"
+                value="dodaj do zamówinia"
+                type="submit"
+                id="addToCartButton"
+              />
+            </div>
+          </form>
+    `;
+    excursionsList.appendChild(excursionItem);
+  });
+}
+
 function renderCart() {
   const cartList = document.querySelector('.panel__summary');
   cartList.innerHTML = '';
@@ -50,45 +113,35 @@ function addToCart(event) {
     '.excursions__field-input[name="children"]'
   );
   const childPriceInput = form.querySelector('.excursions__field--child-price');
+  if (event.submitter && event.submitter.id === 'addToCartButton') {
+    const excursionItem = form.closest('.excursions__item');
+    const title = excursionItem.querySelector('.excursions__title').textContent;
 
-  const excursionItem = form.closest('.excursions__item');
-  const title = excursionItem.querySelector('.excursions__title').textContent;
+    const adults = parseInt(adultsInput.value);
+    const adultPrice = parseInt(adultPriceInput.textContent);
+    const children = parseInt(childrenInput.value);
+    const childPrice = parseInt(childPriceInput.textContent);
 
-  const adults = parseInt(adultsInput.value);
-  const adultPrice = parseInt(adultPriceInput.textContent);
-  const children = parseInt(childrenInput.value);
-  const childPrice = parseInt(childPriceInput.textContent);
-
-  if (isValidQuantity(adults) && isValidQuantity(children)) {
-    alert('Proszę wprowadzić poprawną ilość dorosłych lub dzieci.');
-    return;
-  }
-
-  const totalPrice =
-    (isNaN(adults) ? 0 : adults) * adultPrice +
-    (isNaN(children) ? 0 : children) * childPrice;
-
-  if (totalPrice > 0) {
-    const cartItem = {
-      title,
-      adults: isNaN(adults) ? 0 : adults,
-      adultPrice,
-      children: isNaN(children) ? 0 : children,
-      childPrice,
-      totalPrice,
-    };
-    cart.push(cartItem);
-    resetExcursionsInput(adultsInput, childrenInput);
-    renderCart();
-  } else {
-    alert('Proszę wprowadzić poprawną ilość dorosłych lub dzieci.');
+    const totalPrice =
+      (isNaN(adults) ? 0 : adults) * adultPrice +
+      (isNaN(children) ? 0 : children) * childPrice;
+    if (totalPrice > 0) {
+      const cartItem = {
+        title,
+        adults: isNaN(adults) ? 0 : adults,
+        adultPrice,
+        children: isNaN(children) ? 0 : children,
+        childPrice,
+        totalPrice,
+      };
+      cart.push(cartItem);
+      resetExcursionsInput(adultsInput, childrenInput);
+      renderCart();
+    } else {
+      alert('Proszę wybrać bilet!');
+    }
   }
 }
-
-function isValidQuantity(quantity) {
-  return isNaN(quantity) || quantity <= 0;
-}
-
 function removeCartItem(e) {
   e.preventDefault();
   const itemIndex = e.target.getAttribute('data-item-index');
